912291dd63fee1d9ae9854714d4ef63736ade1f9
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-05-14 13:14:28 -0300

    [Aula-19] Obtendo a carteira de criptomoedas

--- src/main/java/com/softwarefoundation/cryptoapi/controller/CoinController.java
+++ src/main/java/com/softwarefoundation/cryptoapi/controller/CoinController.java
@@ -5,6 +5,7 @@ import com.softwarefoundation.cryptoapi.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,11 @@ public class CoinController {
 
     @Autowired
     private CoinRepository coinRepository;
+
+    @GetMapping
+    public ResponseEntity get() {
+        return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity post(@RequestBody Coin coin) {
--- src/main/java/com/softwarefoundation/cryptoapi/repository/CoinRepository.java
+++ src/main/java/com/softwarefoundation/cryptoapi/repository/CoinRepository.java
@@ -4,7 +4,12 @@ import com.softwarefoundation.cryptoapi.entity.Coin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Slf4j
 @Repository
@@ -12,6 +17,7 @@ import org.springframework.stereotype.Repository;
 public class CoinRepository {
 
     private static String INSERT = "INSERT INTO TB01_COIN ( NAME, PRICE, QUANTITY, DATETIME) VALUES (?,?,?,?)";
+    private static String SELECT_ALL = "SELECT NAME, SUM(QUANTITY) AS QUANTITY FROM TB01_COIN GROUP BY NAME";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -28,5 +34,17 @@ public class CoinRepository {
         };
         this.jdbcTemplate.update(INSERT, attr);
         return coin;
+    }
+
+    public List<Coin> getAll() {
+        return this.jdbcTemplate.query(SELECT_ALL, new RowMapper<Coin>() {
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Coin coin = new Coin();
+                coin.setName(rs.getString("NAME"));
+                coin.setQuantity(rs.getBigDecimal("QUANTITY"));
+                return coin;
+            }
+        });
     }
 }
