adb7e94182c4d941dc801ce19e82d656347417a4
Dherkyan Ribeiro <dherkyan@gmail.com>
2023-05-14 12:58:46 -0300

    [Aula-18] Implementando o cadastro de moedas

---
+++ src/main/java/com/softwarefoundation/cryptoapi/controller/CoinController.java
@@ -0,0 +1,32 @@
+package com.softwarefoundation.cryptoapi.controller;
+
+import com.softwarefoundation.cryptoapi.entity.Coin;
+import com.softwarefoundation.cryptoapi.repository.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/coin")
+public class CoinController {
+
+    @Autowired
+    private CoinRepository coinRepository;
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody Coin coin) {
+        try {
+            coin.setDateTime(new Timestamp(System.currentTimeMillis()));
+            return new ResponseEntity<>(this.coinRepository.insert(coin), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
--- src/main/java/com/softwarefoundation/cryptoapi/repository/CoinRepository.java
+++ src/main/java/com/softwarefoundation/cryptoapi/repository/CoinRepository.java
@@ -1,11 +1,17 @@
 package com.softwarefoundation.cryptoapi.repository;
 
 import com.softwarefoundation.cryptoapi.entity.Coin;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Slf4j
+@Repository
+@EnableAutoConfiguration
 public class CoinRepository {
 
-    private static String INSERT = "INSERT INTO TB01_COIN ( NAME, PRICE, QUANTITY, DATETIME) VALEUS (?,?,?,?)";
+    private static String INSERT = "INSERT INTO TB01_COIN ( NAME, PRICE, QUANTITY, DATETIME) VALUES (?,?,?,?)";
 
     private JdbcTemplate jdbcTemplate;
 
